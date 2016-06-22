b4aeb560ecec2d5e2c1495fad5075aa992343cbe
danielledc <misscauthen@yahoo.com>
2016-06-21 22:37:48 -0400

    updated db

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
-var dbURI = 'mongodb://localhost/test';
-if(process.env.NODE_ENV==="production"){
+//var dbURI = 'mongodb://localhost/test';
+//if(process.env.NODE_ENV==="production"){
 	dbURI='mongodb://heroku_pjnlp2b5:neg22joooa48ucncm29alrn4maj@ds015924.mlab.com:15924/heroku_pjnlp2b5'
-}
+//}
 mongoose.connect(dbURI);
 
 
