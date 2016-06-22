0780ced8a2c857789285684462e0897f29f45609
danielledc <misscauthen@yahoo.com>
2016-06-21 21:37:15 -0400

    added mongodb, googlemaps, and yelp
    Signed-off-by: danielledc <misscauthen@yahoo.com>

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var path = require('path');
+var yelp=require('yelp');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+require('./app_server/models/db');
+require('./app_server/models/yelp');
 
 /*var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -25,7 +28,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/node_modules', express.static(__dirname + '/node_modules/'));
 app.use('/', routes);
 app.use('/users', users);
 
@@ -59,6 +62,10 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+
+app.get('*', function(req, res) {
+        res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    });
 
 
 module.exports = app;
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,11 +1,49 @@
 var express = require('express');
-var router = express.Router();
-var ctrlMain = require('../controllers/main');
+var yelp = require('../models/yelp');
 
+
+var mongoose=require('mongoose');
+var router = express.Router();
+var ctrlOthers = require('../controllers/others');
+
+var locationSchema=new mongoose.Schema({
+	storeName: String,
+	address: String,
+	yelpID: String,
+	phone: String,
+	website: String,
+	hours: String
+	
+	
+	
+}, {collection: 'organicstores'});
+
+var Loc=mongoose.model('store', locationSchema);
 var homepageController= function(req, res){
 	res.render('index', { title: 'Express' });
 }
-/* GET home page. */
-router.get('/', ctrlMain.index);
+
+router.get('/about/', ctrlOthers.about);
+
+router.get("/stores", function(req, res) {
+	 Loc.find(function(err, stores) {
+
+            // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+            if (err)
+                res.send(err)
+
+            res.json(stores); // return all todos in JSON format
+        });
+});
+  
+router.get("/ratings", function(req, res) {
+
+yelp.y.business(req.query.yelpID, function(err, data) {
+						if (err) return console.log(error);
+						console.log(res.json(data));
+  
+						});
+});
+
 
 module.exports = router;
