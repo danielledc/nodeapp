ab636a83451aa06c4c8d7c229a8d36629de5c220
danielledc <misscauthen@yahoo.com>
2016-06-30 12:25:05 -0400

    updated lat and long

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -7,7 +7,7 @@ angular.module('organicStores', [])
     $scope.zipCode="";
     var mapOptions = {
 		 zoom: 11,
-		center:new google.maps.LatLng(40.745572,-74.1051107)
+		center:new google.maps.LatLng(40.799912,-74.01)
     		}
     $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
 		geocoder = new google.maps.Geocoder();
