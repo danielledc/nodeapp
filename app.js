4ac225670a933cf7ad907ad8bccff1745547f0c5
danielledc <misscauthen@yahoo.com>
2016-07-15 11:50:45 -0400

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/node_modules', express.static(__dirname + '/node_modules/'));
-app.use('/', routes);
+app.use('/api/', routes);
 /*app.use('/users', users);*/
 
 // catch 404 and forward to error handler
