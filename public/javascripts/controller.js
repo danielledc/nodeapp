2bfea09d1cd47a57b4bc3a70050179761ec6477f
danielledc <misscauthen@yahoo.com>
2017-07-14 02:37:28 -0400

    Update controller.js

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -48,6 +48,7 @@ var organicStores = angular.module('organicStores', ['ngRoute','angularSpinners'
 			$scope.clickedOnMap = false;
 			
 			$.each(data, function(key,value) {    
+				     console.log(value.yelpID);
 					$http.get('/api/ratings?yelpID='+value.yelpID)
 						.then(function(data){
 								$scope.stores[key].rating=data.rating;
