62e87aa69f5c32f10667f85838f1a54a7bc4c56b
danielledc <misscauthen@yahoo.com>
2017-07-14 02:20:39 -0400

    changed token

--- app_server/models/yelp.js
+++ app_server/models/yelp.js
@@ -4,8 +4,8 @@ var Yelp = require('yelp');
 var yelp = new Yelp({
   consumer_key: '5P2TUxZ_4SNCBgvm2H43TQ',
   consumer_secret: 'dlHSj5s2z1ge4dG7aIMYZn345J4',
-  token: 'hPXOoaWn6YcZuTKcL7ZEcMwuWFjzDvZa',
-  token_secret: 'lKv-Rw3yUFj4SjD8xuQhB5IRC2Q',
+  token: 'F_5Oj6A8lnh9Kmv1jPuDkPJi-pcKcvlC',
+  token_secret: '1fQHYApunKXu9693rvUGQqo20Dk',
 });
 // A callback based API is also available:
 
