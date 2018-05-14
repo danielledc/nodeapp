77d59d2a30d839922699e2496b6f9e93b2c807ab
Danielle <misscauthen@yahoo.com>
2018-05-14 17:00:01 -0400

    debugging

--- app_server/models/yelp.js
+++ app_server/models/yelp.js
@@ -3,10 +3,7 @@ const yelp = require('yelp-fusion');
 
 const apiKey = 'EtWiGICoPPmtLt1j6CqQe-yYpLm2O4AVkGvKccKE95Qbg4Mo0rDPLHMx6H8BuHrT8cDnV_QCxv3dodL3hR8XNireT5TTHYQpewpaJy0OoZXlzphr-dOrJfnWucn5WnYx';
 
-
-const client = yelp.client(apiKey);
-
-console.log(client);
+console.log(yelp.client(apiKey));
 // A callback based API is also available:
 
-module.exports.y = client;
+module.exports.y = yelp.client(apiKey);
